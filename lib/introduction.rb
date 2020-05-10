3d17f15505dc9323c0d62d7ff1edd9b842a1e445
Jay Roth <253jay@gmail.com>
2020-05-10 22:25:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,8 @@
 
 def introduction(josh)
   puts "Hi, my name is #{josh}."
+  end
+
+def introduction_with_language(dan, emberjs)
+  puts "Hi, my name is #{dan} and I am learning to program in #{emberjs}."
 end
--- lib/introduction_with_language.rb
+++
@@ -1,5 +0,0 @@
-# introduction_with_language.rb
-
-def introduction_with_language(dan, ember.js.)
- puts "Hi, my name is #{dan} and I am learning to program in #{ember.js.}."
- end
